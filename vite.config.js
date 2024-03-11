0eef391c15c1e279c4771e97826f0740a56493a6
sakura-zzs <73648480+sakura-zzs@users.noreply.github.com>
2024-03-12 00:36:34 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -42,12 +42,10 @@ export default defineConfig({
     }
   },
   server: {
-    // http://localhost:5173/api/login -> http://kuroneko.icu:8001/login
     proxy: {
       //解决本地代理错误的问题
       //api是自行设置的请求前缀，任何请求路径以/api开头的请求将被代理到对应的target目标
       '/api': {
-        // target: 'http://kuroneko.icu:8001', //目标域名
         target: 'http://localhost:8001', //目标域名
         changeOrigin: true, //需要代理跨域
         rewrite: (path) => path.replace(/^\/api/, '') //路径重写，把'/api'替换为''
